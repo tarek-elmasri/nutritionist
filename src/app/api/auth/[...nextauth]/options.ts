7bb2dd26c0e3ba938eabeb49ff1a43b3418652b4
tarek-elmasri <tarek.elmasri201@gmail.com>
2023-08-03 00:40:46 +0300

    [fix] prisma client path

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,7 +3,7 @@ import { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/libs/prisma";
+import prisma from "@/lib/prisma";
 
 const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
