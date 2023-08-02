5f9a0de88292ac3006010c211bc055a5872a1342
tarek-elmasri <tarek.elmasri201@gmail.com>
2023-08-03 00:35:35 +0300

    [feat] implements next-auth

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,86 @@
+import bcrypt from "bcrypt";
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/libs/prisma";
+
+const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid Credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.password) {
+          throw new Error("Invalid Credentials");
+        }
+
+        const salt = process.env.SALT as string;
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password + salt,
+          user.password
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid Credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    session: ({ token, session }) => {
+      if (token) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+        session.user.name = token.name;
+        session.user.isAdmin = token.isAdmin;
+      }
+      return session;
+    },
+    jwt: async ({ token }) => {
+      const dbUser = await prisma.user.findFirst({
+        where: { email: token.email! },
+      });
+
+      if (!dbUser) {
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        email: dbUser.email,
+        isAdmin: dbUser.isAdmin,
+        name: dbUser.name,
+        image: dbUser.image,
+      };
+    },
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default authOptions;
