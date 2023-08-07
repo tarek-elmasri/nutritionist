56add130e17303a3f5f2e4ddf29595ca742496d1
tarek-elmasri <tarek.elmasri201@gmail.com>
2023-08-07 11:28:48 +0300

    [fix] duplicate creations in seeds

--- prisma/seeds/index.js
+++ prisma/seeds/index.js
@@ -3,8 +3,14 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 const seed = async () => {
+  await prisma.user.createMany({})
+  await prisma.profile.deleteMany({})
+  await prisma.subscriptionPlan.deleteMany({})
+
+
+
   // create subscription plans
-  await prisma.subscriptionPlan.create(
+  await prisma.subscriptionPlan.createMany(
     {
       data: {
         label: "1 Week",
@@ -14,39 +20,38 @@ const seed = async () => {
 
       }
 
+    },
+    {
+      data: {
+        label: "1 Month",
+        price: 25,
+        per: "month",
+        description: "Billed $25 monthly"
+
+      }
+    },
+    {
+      data: {
+        label: "6 Month",
+        price: 120,
+        per: "6 Month",
+        description: "Billed $60 querterly"
+      }
+    },
+
+    {
+      data: {
+
+        label: "12 Month",
+        price: 220,
+        per: "12 Month",
+        description: "Billed $120 semisterly"
+
+      }
     })
-
-  await prisma.subscriptionPlan.create({
-    data: {
-      label: "1 Month",
-      price: 25,
-      per: "month",
-      description: "Billed $25 monthly"
-
-    }
-  })
-
-  await prisma.subscriptionPlan.create({
-    data: {
-      label: "6 Month",
-      price: 120,
-      per: "6 Month",
-      description: "Billed $60 querterly"
-    }
-  })
-
-  await prisma.subscriptionPlan.create({
-    data: {
-
-      label: "12 Month",
-      price: 220,
-      per: "12 Month",
-      description: "Billed $120 semisterly"
-
-    }
-  })
 
 
 }
+
 
 seed()
