949d2ef770d0ea071bff03d7fa5ca4c7edf15eef
tarek-elmasri <tarek.elmasri201@gmail.com>
2023-08-07 10:59:51 +0300

    [feat] adds serves constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 import tabs from "@/constants/tabs";
+import servesConstants from "@/constants/serves";
 
 export const TABS = tabs;
+export const serves = servesConstants;
---
+++ src/constants/serves.ts
@@ -0,0 +1,102 @@
+import { ServeType } from "@/type";
+
+export const fruitServe: ServeType = {
+  CHO: 15,
+  protien: 0,
+  fat: 0,
+  calories: 60,
+};
+
+export const legumeServe: ServeType = {
+  CHO: 15,
+  protien: 7,
+  fat: 0,
+  calories: 110,
+};
+
+export const leanMeatServe: ServeType = {
+  CHO: 0,
+  protien: 7,
+  fat: 3,
+  calories: 45,
+};
+
+export const mediumMeatServe: ServeType = {
+  CHO: 0,
+  protien: 7,
+  fat: 5,
+  calories: 75,
+};
+
+export const highMeatServe: ServeType = {
+  CHO: 0,
+  protien: 7,
+  fat: 8,
+  calories: 100,
+};
+
+export const freeFatMilkServe: ServeType = {
+  CHO: 15,
+  protien: 8,
+  fat: 3,
+  calories: 100,
+};
+
+export const mediumFatMilkServe: ServeType = {
+  CHO: 15,
+  protien: 8,
+  fat: 5,
+  calories: 120,
+};
+
+export const highFatMilkServe: ServeType = {
+  CHO: 15,
+  protien: 8,
+  fat: 8,
+  calories: 150,
+};
+
+export const starchServe: ServeType = {
+  CHO: 15,
+  protien: 3,
+  fat: 0,
+  calories: 80,
+};
+
+export const vegetableServe: ServeType = {
+  CHO: 5,
+  protien: 2,
+  fat: 0,
+  calories: 25,
+};
+
+export const sugarServe: ServeType = {
+  CHO: 15,
+  protien: 0,
+  fat: 0,
+  calories: 60,
+};
+
+export const oilServe: ServeType = {
+  CHO: 0,
+  protien: 0,
+  fat: 5,
+  calories: 45,
+};
+
+const serves = {
+  freeFatMilkServe,
+  fruitServe,
+  highFatMilkServe,
+  highMeatServe,
+  leanMeatServe,
+  legumeServe,
+  mediumFatMilkServe,
+  mediumMeatServe,
+  oilServe,
+  starchServe,
+  sugarServe,
+  vegetableServe,
+};
+
+export default serves;
