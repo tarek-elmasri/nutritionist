84de5d6d00a17877b4327628e715cefbb326dbbd
tarek-elmasri <tarek.elmasri201@gmail.com>
2023-08-13 19:32:05 +0300

    [feat] adds notifications menu and corresponding actions

---
+++ src/actions/getNotifications.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import getCurrentUser from "./get CurrentUser";
+
+export const getNotifications = async (limit: number = 10, offset?: number) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser)
+    return { notifications: [], hasMore: false, newNotificationsCount: 0 };
+
+  const notifications = await prisma.notification.findMany({
+    where: {
+      userId: currentUser.id,
+    },
+    take: limit + 1,
+    skip: offset,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const newNotificationsCount = await prisma.notification.count({
+    where: {
+      userId: currentUser.id,
+      seen: false,
+    },
+  });
+
+  const hasMore = notifications.length === limit + 1;
+  if (hasMore) notifications.pop();
+
+  return {
+    newNotificationsCount,
+    notifications,
+    hasMore,
+  };
+};
--- src/components/notifications-menu.tsx
+++ src/components/notifications-menu.tsx
@@ -1,13 +1,146 @@
 "use client";
 
 import { Bell } from "lucide-react";
-import React from "react";
+import { Notification } from "@prisma/client";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components//ui/popover";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { FC, useEffect, useState } from "react";
+import useFetch from "@/hooks/useFetch";
+import { getNotifications } from "@/actions/getNotifications";
+import updateNotification from "@/actions/updateNotification";
+import PageLoader from "@/components/ui/page-loader";
+
+interface NotificationCellProps {
+  seen: boolean;
+  label: string;
+  onClick?: () => void;
+}
+
+const NotificationCell: FC<NotificationCellProps> = ({
+  seen,
+  label,
+  onClick,
+}) => (
+  <li
+    className={cn(
+      "p-3 w-full border-b border-b-neutral-300 cursor-pointer hover:bg-lightgreen",
+      !seen && "bg-lightgreen/40"
+    )}
+  >
+    <div className="w-full" role="link" onClick={onClick}>
+      {label}
+    </div>
+  </li>
+);
+
+const NoNotifications = () => (
+  <div className="h-[10rem] grid place-items-center">
+    <p>No Notifications.</p>
+  </div>
+);
 
 const NotificationsMenu = () => {
+  const router = useRouter();
+  const NOTIFICATION_LIMIT = 10;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [notificationList, setNotificationList] = useState<Notification[]>([]);
+  const [newNotificationsCount, setNewNotificationsCount] = useState(0);
+  const [notificationsOffset, setNotificationsOffset] = useState(0);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const { data, refetch } = useFetch(() =>
+    getNotifications(NOTIFICATION_LIMIT, notificationsOffset)
+  );
+
+  useEffect(() => {
+    if (data && data.notifications) {
+      setNotificationList((prev) => [...prev, ...data.notifications]);
+      setNewNotificationsCount(data.newNotificationsCount);
+    }
+  }, [data, setNotificationList, setNewNotificationsCount]);
+
+  const handleNavigation = async (
+    notification: Notification,
+    index: number
+  ) => {
+    // set menu closed
+    setIsMenuOpen(false);
+    try {
+      // if seen is false trigger action to turn it true
+      if (notification.seen === false) {
+        setIsUpdating(true);
+        await updateNotification(notification.id);
+        // update current notification list and new counts
+        const newList = [...notificationList];
+        newList[index].seen = true;
+        setNotificationList(newList);
+        setNewNotificationsCount((prev) => prev - 1);
+      }
+      // redirect to href
+      router.push(notification.href);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
   return (
-    <div className="p-3 rounded-full shadow-primary hover:bg-lightgreen/40 hover:shadow-md focus-visible:bg-lightgreen/40 focus-visible:shadow-md grid place-content-center cursor-pointer">
-      <Bell className="w-6 h-6 text-neutral-600" />
-    </div>
+    <>
+      {isUpdating && <PageLoader />}
+      <Popover open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            className="relative rounded-full p-0 w-12 h-12 hover:bg-lightgreen focus-visible:bg-lightgreen"
+          >
+            <Bell className="w-6 h-6 text-neutral-600" />
+            {newNotificationsCount > 0 && (
+              <div className="absolute top-0 right-0 w-4 aspect-square rounded-full bg-red-500 flex justify-center items-center">
+                <p className="text-xs text-white">{newNotificationsCount}</p>
+              </div>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent
+          className="w-80 h-[15rem] overflow-y-auto p-0"
+          align="end"
+        >
+          <ul className="w-full bg-background [&_li:last-child]:border-b-0">
+            {notificationList.length === 0 ? (
+              <NoNotifications />
+            ) : (
+              notificationList.map((notification, index) => (
+                <NotificationCell
+                  seen={notification.seen}
+                  label={notification.label}
+                  key={notification.id}
+                  onClick={() => handleNavigation(notification, index)}
+                />
+              ))
+            )}
+
+            {data?.hasMore && (
+              <NotificationCell
+                seen={true}
+                label="View More"
+                onClick={() => {
+                  setNotificationsOffset(
+                    (prev) => prev + NOTIFICATION_LIMIT - 1
+                  );
+                  refetch();
+                }}
+              />
+            )}
+          </ul>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 };
 
